342d6729a85285150fbc3060bffa29967299418f
Vodolay <ivanovi@vaniercollege.qc.ca>
2019-05-05 09:32:10 -0400

    Update NN_trajectories_auto.py
    The training module

--- NN_trajectories_auto.py
+++ NN_trajectories_auto.py
@@ -9,27 +9,39 @@ import random
 
 torch.set_default_dtype(torch.float64)
 
-xi = -20. #left endpont
-xf = 20. #right endpoint
-J = 5001 #number of space nodes
-Xgrid = np.linspace(xi, xf, J)
+nt = 500
+N = 1000
 
-x = np.load('tracks/gauss1.npy')
-x = x[:, :, 200:]
+def files(path):
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            yield file
+            
+directory = "tracks"
 
-nt = x.shape[1]
-N = x.shape[2]
+asps = []
 
-batch_size = 60
+for file in files("./{}/".format(directory)):
+    asps.append(file)
+
+length = len(asps)
+
+z = np.zeros([length, 2, nt, N])
+for i in range(length):
+    y = np.load("./{}/{}".format(directory, asps[i]))
+    z[i, :, :, :] = y 
+    
+batch_size = 200
 # make a random batch
 
-def batch(x):
+def batch(z):
     batchy = np.zeros([batch_size, 2, nt, 9])
     df = np.zeros([batch_size, nt])
     for i in range(batch_size):
+        j = np.random.randint(length)
         k = np.random.randint(0, N-9)
-        batchy[i, :, :, :] = x[:, :, k:k+9]
-        df[i, :] = x[0, :, k+9] - x[0, :, k+8]
+        batchy[i, :, :, :] = z[j, :, :, k:k+9]
+        df[i, :] = z[j, 0, :, k+9] - z[j, 0, :, k+8]
     return batchy, df
 
 class TNet(nn.Module):
@@ -65,44 +77,30 @@ net = TNet()
 net = net.cuda()
 net.load_state_dict(torch.load('saves/tnet'))
 criterion = nn.MSELoss()
-optimizer = optim.Adam(net.parameters(), lr=0.00006)
+optimizer = optim.Adam(net.parameters(), lr=0.0001)
 
-def files(path):
-    for file in os.listdir(path):
-        if os.path.isfile(os.path.join(path, file)):
-            yield file
 
-directory = "tracks"
+for epoch in range(0, 900_000+1):
 
-asps = []
-
-for file in files("./{}/".format(directory)):
-    asps.append(file)
-
-for epoch in range(0, 500_000+1):
-    ranfi = random.choice(asps)
-    x = np.load(("./{}/".format(directory) + ranfi))
-
-    for i in range(1000):
-        optimizer.zero_grad()
+    optimizer.zero_grad()
     
-        data, df = batch(x)
-        data = torch.from_numpy(data).cuda()
-        df = torch.from_numpy(df).cuda()
+    data, df = batch(z)
+    data = torch.from_numpy(data).cuda()
+    df = torch.from_numpy(df).cuda()
         
-        output = net(data)
+    output = net(data)
 
-        loss = 1000000*criterion(output, df)
-        loss.backward()
-        optimizer.step()
+    loss = 1000000*criterion(output, df)
+    loss.backward()
+    optimizer.step()
         
-    if epoch % 3 == 0:
-        print("file name:" + ranfi)
+    if epoch % 100 == 0:
         print("Epoch "+ str(epoch) + ": "+("%.9f" % loss.data.item()))
 
     
-    if epoch % 3 == 0:
+    if epoch % 100 == 0:
         for g in optimizer.param_groups:
-            g['lr'] = g['lr'] / 1.03
+            g['lr'] = g['lr'] / 1.003
+        print("learning rate {:4f}".format(g['lr']))
         torch.save(net.state_dict(), "saves/tnet")
 
